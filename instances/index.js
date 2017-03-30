2297d32965857bef87ca85ff18e84d3dc42f40e3
HDYA <hdya_backfire@sina.cn>
2017-03-30 17:53:31 +0800

    Implement instance nodejs program

---
+++ instances/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var uuid = require('node-uuid');  
+var instance_id = uuid.v4();
+
+var default_port = 8888;
+var move;
+
+// Choose one of the following two sentence to comment
+move = true;
+//move = false;
+
+app.get('/move', function(req, res) {
+    res.set({'Content-Type':'text/json','Encodeing':'utf8'});
+    res.send({
+        instance: instance_id,
+        move: move,
+    });
+});
+
+console.log('Instance', instance_id, 'starts to listen on port', default_port, 'with move setting', move);
+app.listen(default_port);
