bb27241ff9bf8f4887942a5d7e84706a0b21764e
HDYA <hdya_backfire@sina.cn>
2017-04-01 19:34:36 +0800

    Use os name for instance id

--- instances/index.js
+++ instances/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
-var uuid = require('node-uuid');  
-var instance_id = uuid.v4();
+var os = require('os');
 
 var default_port = 8080;
 var move;
@@ -13,10 +12,10 @@ move = true;
 app.get('/move', function(req, res) {
     res.set({'Content-Type':'text/json'});
     res.send({
-        instance: instance_id,
+        instance: os.hostname(),
         move: move,
     });
 });
 
-console.log('Instance', instance_id, 'starts to listen on port', default_port, 'with move setting', move);
+console.log('Instance', os.hostname(), 'starts to listen on port', default_port, 'with move setting', move);
 app.listen(default_port);
